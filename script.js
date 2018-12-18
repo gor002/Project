1b76e4d9655d3e4db2dd3197f9308736215447da
Your Name <you@example.com>
2018-12-18 19:35:08 +0400

    sa

--- script.js
+++ script.js
@@ -8,28 +8,42 @@ function setup() {
 socket = io();
 side = 15;
 
-function drawMatrix(matrix) {
 
-    console.log(matrix);
-    for (var y = 0; y < matrix.length; y++) {
-        for (var x = 0; x < matrix[y].length; x++) {
 
-            if (matrix[y][x] == 1) {
-                fill("green");
+
+function drawMatrix(obj) {
+
+    console.log(obj.exanakNow);
+    for (var y = 0; y < obj.matrix.length; y++) {
+        for (var x = 0; x < obj.matrix[y].length; x++) {
+            
+            if(obj.matrix[y][x] == 1 && obj.exanakNow == "Dzmer"){
+                fill("white");
             }
-            if (matrix[y][x] == 2) {
+            if(obj.matrix[y][x] == 1 && obj.exanakNow == "Garun"){
+                fill("GreenYellow");
+            }
+            if(obj.matrix[y][x] == 1 && obj.exanakNow == "Amar"){
+                fill("Orange");
+            }
+
+            if(obj.matrix[y][x] == 1 && obj.exanakNow == "Ashun"){
+                fill("brown");
+            }
+
+            if (obj.matrix[y][x] == 2) {
                 fill("yellow");
             }
-            if (matrix[y][x] == 3) {
+            if (obj.matrix[y][x] == 3) {
                 fill("red");
             }
-            if (matrix[y][x] == 4) {
+            if (obj.matrix[y][x] == 4) {
                 fill("blue");
             }
-            if (matrix[y][x] == 5) {
+            if (obj.matrix[y][x] == 5) {
                 fill("black");
             }
-            else if (matrix[y][x] == 0) {
+            else if (obj.matrix[y][x] == 0) {
                 fill("#acacac");
             }
 
@@ -46,4 +60,4 @@ function drawMatrix(matrix) {
 }
 
 
-socket.on("matrix", drawMatrix);
+socket.on("send matrix", drawMatrix);
--- server.js
+++ server.js
@@ -14,8 +14,8 @@ matrix = [];
 
 
 
-io.on('connection', function (socket) { 
- 
+io.on('connection', function (socket) {
+
 });
 var n = 60;
 var m = 60;
@@ -74,14 +74,92 @@ for (var y = 0; y < matrix.length; y++) {
 
 }
 
+// exanakNow = "Dzmer"
 
 
+// function exanak() {
+//     if (exanakNow == "Ashun") {
+//         exanakNow = "Dzmer";
+//     }
+//     if (exanakNow == "Dzmer") {
+//         exanakNow = "Garun";
+//     }
+//     if (exanakNow == "Garun") {
+//         exanakNow = "Amar";
+//     }
+//     if (exanakNow == "Amar") {
+//         exanakNow = "Ashun";
+//     }
+//     //io.sockets.emit("exanakNow" , exanakNow)
 
+// }
+
+// setInterval(exanak, 6000)
+
+
+// function drawServerayin() {
+
+//     for (var i in grassArr) {
+//         grassArr[i].mult();
+//     }
+//     for (var i in xotakerArr) {
+//         xotakerArr[i].mult();
+//         xotakerArr[i].move();
+//         xotakerArr[i].eat();
+//         xotakerArr[i].die();
+
+
+//     }
+//     for (var i in gishatichArr) {
+//         gishatichArr[i].mult();
+//         gishatichArr[i].eat();
+//         gishatichArr[i].move();
+//         gishatichArr[i].die();
+
+
+//     }
+//     for (var i in kerpar1Arr) {
+//         kerpar1Arr[i].mult();
+//         kerpar1Arr[i].eat();
+//         kerpar1Arr[i].move();
+//         kerpar1Arr[i].die();
+
+
+//     }
+//     for (var i in kerpar2Arr) {
+//         //kerpar1Arr[i].mult();
+//         kerpar2Arr[i].eat();
+//         kerpar2Arr[i].move();
+//         kerpar2Arr[i].die();
+
+
+//     }
+//     //console.log(matrix);
+//     io.sockets.emit("matrix", matrix, exanakNow);
+
+// }
+
+// setInterval(drawServerayin, 200);
+
+
+exanakNow = "Dzmer"
+
+
+function wheter(){
+    if (exanakNow == "Dzmer") {
+        exanakNow = "Garun";
+    }
+    else if (exanakNow == "Garun") {
+        exanakNow = "Amar";
+    }
+    else if (exanakNow == "Amar") {
+        exanakNow = "Ashun";
+    }
+    else if (exanakNow == "Ashun") {
+        exanakNow = "Dzmer";
+    }
+}
 function drawServerayin() {
-
-
-
-
 
     for (var i in grassArr) {
         grassArr[i].mult();
@@ -115,15 +193,12 @@ function drawServerayin() {
         kerpar2Arr[i].eat();
         kerpar2Arr[i].move();
         kerpar2Arr[i].die();
-
-
     }
-    //console.log(matrix);
-    io.sockets.emit("matrix", matrix);
 
+
+
+
+    io.sockets.emit('send matrix', { "matrix": matrix, "exanakNow": exanakNow });
 }
-
-setInterval(drawServerayin, 500);
-
-
-
+setInterval(wheter,4000);
+setInterval(drawServerayin,200);
